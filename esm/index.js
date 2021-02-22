0d31204375cab1da921fabc3c7fb775da04484d6
Cristiano Betta <cristiano@betta.io>
2021-02-22 23:52:42 +0000

    Add logging and mock JWT auto-gen

--- esm/index.js
+++ esm/index.js
@@ -1,4 +1,10 @@
 import Gr4vy from '../dist/index.js'
+
+export const Buyer = Gr4vy.Buyer
+export const Buyers = Gr4vy.Buyers
+export const BuyerCreateRequest = Gr4vy.BuyerCreateRequest
+export const BuyerUpdateRequest = Gr4vy.BuyerUpdateRequest
+
 export const Client = Gr4vy.Client
 
 export default Gr4vy
