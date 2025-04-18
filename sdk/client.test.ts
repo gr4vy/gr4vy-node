c8f1572607f76a5592689de68413747e70a8f114
Cristiano Betta <cbetta@gr4vy.com>
2025-04-18 13:49:53 +0100

    feat(webhook): Add webhook signature validation helper

--- sdk/client.test.ts
+++ sdk/client.test.ts
@@ -193,3 +193,121 @@ describe('.setMerchantAccountId()', () => {
     ).toEqual('test')
   })
 })
+
+describe('.verifyWebook()', () => {
+  test('it should verify webhook happy case', async () => {
+    const secret =
+      'Ik4L-8FH0ihWczctcIPXZRR_8F0fPNgmhEfVBbZ3zNwqQVa1Or4tBz4Pgw2eNaVDod7H56Y268h_wohEUaWbUg'
+    const signatureHeader =
+      '78aca0c78005107a654a957b8566fa6e0e5e06aea92d7da72a6da9e5a690d013,other'
+    const timestampHeader = '1744018920'
+    const payload = 'payload'
+    const timestampTolerance = 0 // no timestamp validation
+
+    Client.verifyWebhook(
+      payload,
+      secret,
+      signatureHeader,
+      timestampHeader,
+      timestampTolerance
+    )
+  })
+
+  test('it should raise an error for an old timestamp', async () => {
+    const secret =
+      'Ik4L-8FH0ihWczctcIPXZRR_8F0fPNgmhEfVBbZ3zNwqQVa1Or4tBz4Pgw2eNaVDod7H56Y268h_wohEUaWbUg'
+    const signatureHeader =
+      '78aca0c78005107a654a957b8566fa6e0e5e06aea92d7da72a6da9e5a690d013,other'
+    const timestampHeader = '1744018920'
+    const payload = 'payload'
+    const timestampTolerance = 60 // 1 minute tolerance
+
+    expect(() => {
+      Client.verifyWebhook(
+        payload,
+        secret,
+        signatureHeader,
+        timestampHeader,
+        timestampTolerance
+      )
+    }).toThrow('Timestamp too old')
+  })
+
+  test('it should raise an error for a wrong signature', async () => {
+    const secret =
+      'Ik4L-8FH0ihWczctcIPXZRR_8F0fPNgmhEfVBbZ3zNwqQVa1Or4tBz4Pgw2eNaVDod7H56Y268h_wohEUaWbUg'
+    const signatureHeader = 'other'
+    const timestampHeader = '1744018920'
+    const payload = 'payload'
+    const timestampTolerance = 0 // no timestamp validation
+
+    expect(() => {
+      Client.verifyWebhook(
+        payload,
+        secret,
+        signatureHeader,
+        timestampHeader,
+        timestampTolerance
+      )
+    }).toThrow('No matching signature found')
+  })
+
+  test('it should raise an error for an invalid timestamp', async () => {
+    const secret =
+      'Ik4L-8FH0ihWczctcIPXZRR_8F0fPNgmhEfVBbZ3zNwqQVa1Or4tBz4Pgw2eNaVDod7H56Y268h_wohEUaWbUg'
+    const signatureHeader =
+      '78aca0c78005107a654a957b8566fa6e0e5e06aea92d7da72a6da9e5a690d013,other'
+    const timestampHeader = 'wrong'
+    const payload = 'payload'
+    const timestampTolerance = 0 // no timestamp validation
+
+    expect(() => {
+      Client.verifyWebhook(
+        payload,
+        secret,
+        signatureHeader,
+        timestampHeader,
+        timestampTolerance
+      )
+    }).toThrow('Invalid header timestamp')
+  })
+
+  test('it should raise an error for a missing signature header', async () => {
+    const secret =
+      'Ik4L-8FH0ihWczctcIPXZRR_8F0fPNgmhEfVBbZ3zNwqQVa1Or4tBz4Pgw2eNaVDod7H56Y268h_wohEUaWbUg'
+    const signatureHeader = null
+    const timestampHeader = 'wrong'
+    const payload = 'payload'
+    const timestampTolerance = 0 // no timestamp validation
+
+    expect(() => {
+      Client.verifyWebhook(
+        payload,
+        secret,
+        signatureHeader,
+        timestampHeader,
+        timestampTolerance
+      )
+    }).toThrow('Missing header values')
+  })
+
+  test('it should raise an error for a missing timestamp header', async () => {
+    const secret =
+      'Ik4L-8FH0ihWczctcIPXZRR_8F0fPNgmhEfVBbZ3zNwqQVa1Or4tBz4Pgw2eNaVDod7H56Y268h_wohEUaWbUg'
+    const signatureHeader =
+      '78aca0c78005107a654a957b8566fa6e0e5e06aea92d7da72a6da9e5a690d013,other'
+    const timestampHeader = null
+    const payload = 'payload'
+    const timestampTolerance = 0 // no timestamp validation
+
+    expect(() => {
+      Client.verifyWebhook(
+        payload,
+        secret,
+        signatureHeader,
+        timestampHeader,
+        timestampTolerance
+      )
+    }).toThrow('Missing header values')
+  })
+})
--- sdk/client.ts
+++ sdk/client.ts
@@ -1,3 +1,4 @@
+import crypto from 'crypto'
 import {
   BuyersApi,
   MerchantAccountsApi,
@@ -402,6 +403,49 @@ class Client {
   private log(...args: any[]) {
     if (this.debug) {
       console.debug(...args)
+    }
+  }
+
+  /**
+   * Verifies a webhook signature
+   *
+   * @param payload The payload that was sent in the webhook
+   * @param secret The secret that was used to sign the webhook
+   * @param signatureHeader The header that contains the signature
+   * @param timestampHeader The header that contains the timestamp
+   * @param timestampTolerance The tolerance for the timestamp
+   */
+  public static verifyWebhook(
+    payload: string,
+    secret: string,
+    signatureHeader: string | null | undefined,
+    timestampHeader: string | null | undefined,
+    timestampTolerance: number
+  ): void {
+    if (!signatureHeader || !timestampHeader) {
+      throw new Error('Missing header values')
+    }
+
+    const timestamp = parseInt(timestampHeader, 10)
+    if (isNaN(timestamp)) {
+      throw new Error('Invalid header timestamp')
+    }
+
+    const signatures = signatureHeader.split(',')
+    const expectedSignature = crypto
+      .createHmac('sha256', secret)
+      .update(`${timestamp}.${payload}`)
+      .digest('hex')
+
+    if (!signatures.includes(expectedSignature)) {
+      throw new Error('No matching signature found')
+    }
+
+    if (
+      timestampTolerance &&
+      timestamp < Date.now() / 1000 - timestampTolerance
+    ) {
+      throw new Error('Timestamp too old')
     }
   }
 }
