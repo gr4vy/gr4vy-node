e3cccaa7667d98fe54fd12a9b869903281435ed1
Cristiano Betta <cbetta@gr4vy.com>
2025-07-04 17:47:52 +0100

    chore: Update openapi source

--- openapi-generator-generate.sh
+++ openapi-generator-generate.sh
@@ -1,7 +1,7 @@
 rm -rf api model
 docker run --rm \
   -v ${PWD}:/local openapitools/openapi-generator-cli:v7.6.0 generate \
-  -i https://gr4vy.github.io/gr4vy-openapi/openapi.sdks.v1.json \
+  -i https://gr4vy.github.io/openapi/openapi.sdks.v1.json \
   -g typescript-node \
   --git-user-id gr4vy \
   --git-repo-id gr4vy-node \
